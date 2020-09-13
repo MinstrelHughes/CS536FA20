94edd2c387b9ff2f9bdc51bd99c6a9fdc67b83d6
MinstrelHughes <zhuoran4@illinois.edu>
2020-09-13 12:58:12 -0400

    main

--- lab1/client.py
+++ lab1/client.py
@@ -1,23 +1,35 @@
 import socket
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
-DATA_LEN = 2048
-CODE = "utf-8" 
+import sys
+# specify host, port, data amount and code style
+def main(argv):
+	if len(argv)==2:
+		HOST = argv[0]
+		PORT = int(argv[1])
+	else:
+		print("Invalid arguments. Run on default.")
+		HOST = '127.0.0.1'  
+		PORT = 3000  
 
-sock = socket.socket()
-server_addr = (HOST,PORT)
+	DATA_LEN = 2048
+	CODE = "utf-8" 
 
-try:
-	sock.connect(server_addr)
-	sent_data = raw_input("Input> ")
-	while sent_data!="QUIT" and sent_data:
-		sock.sendall(sent_data.encode(CODE))
-		received_data = sock.recv(DATA_LEN)
-		print "Received: ", received_data.decode(CODE)
+	sock = socket.socket()
+	server_addr = (HOST,PORT)
+
+	try:
+		sock.connect(server_addr)
 		sent_data = raw_input("Input> ")
-except socket.gaierror as e:
-	print str(e)
-except Exception as e:
-	print e
-finally:
-	sock.close()
+		while sent_data!="QUIT" and sent_data:
+			sock.sendall(sent_data.encode(CODE))
+			received_data = sock.recv(DATA_LEN)
+			print "Received: ", received_data.decode(CODE)
+			sent_data = raw_input("Input> ")
+	except socket.gaierror as e:
+		print str(e)
+	except Exception as e:
+		print e
+	finally:
+		sock.close()
+
+if __name__=="__main__":
+	main(sys.argv[1:])
--- lab1/server.py
+++ lab1/server.py
@@ -1,44 +1,55 @@
-HOST = '127.0.0.1'  
-PORT = 65432  
-DATA_LEN = 2048
-CODE = "utf-8" 
-WELCOME_MSG = b"Welcome"     
 import socket
 import threading
+import sys
+def main(argv):
+    HOST = "127.0.0.1"
+    # specify host, port, data amount, etc.
+    print(argv)
+    if len(argv)!=1:
+        print("Invalid arguments. Run on default port 3000")
+        PORT = 3000  
+    else:
+        PORT = int(argv[0])
+    DATA_LEN = 2048
+    CODE = "utf-8" 
+    WELCOME_MSG = b"Welcome" 
+
+    # threading to handle a client function
+    def tcplink(sock, address):
+        print('Connection from %s:%s started.' % address)    
+        while True:
+            data = sock.recv(DATA_LEN)
+            msg = data.decode(CODE)
+            # receive data, make judgement on sepcial messages
+            print("Received: %s" % msg)
+            print("from %s:%s" %address)
+            if not data or msg == 'QUIT':
+                break
+            if msg== "HELLO":
+                sock.sendall(WELCOME_MSG)
+            else:
+                sock.sendall(data)
+
+        sock.close()
+        print('Connection from %s:%s closed.' % address)    
 
 
-def tcplink(sock, address):
-    print('Connection from %s:%s started.' % address)    
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+    # bind host and port
+    sock.bind((HOST,PORT))
+    # start to listen
+    sock.listen(5)
+    print("Server Start")
     while True:
-        data = sock.recv(DATA_LEN)
-        msg = data.decode(CODE)
-        # receive data, make judgement on sepcial messages
-        print("Received: %s" % msg)
-        print("from %s:%s" %address)
-        if not data or msg == 'QUIT':
-            break
-        if msg== "HELLO":
-            sock.sendall(WELCOME_MSG)
-        else:
-            sock.sendall(data)
+        # accept from client and create a thread to handle it
+        client, address = sock.accept()
+        t = threading.Thread(target=tcplink, args=(client, address))
+        t.start()
+    client.close()
 
-    sock.close()
-    print('Connection from %s:%s closed.' % address)    
-
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-# bind host and port
-sock.bind((HOST,PORT))
-# start to listen
-sock.listen(5)
-print("Server Start")
-while True:
-    # accept from client and create a thread to handle it
-    client, address = sock.accept()
-    t = threading.Thread(target=tcplink, args=(client, address))
-    t.start()
-client.close()
+if __name__=="__main__":
+    main(sys.argv[1:])
 
 
 
